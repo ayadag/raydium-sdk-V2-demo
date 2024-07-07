5eb27f8cd6e78e05d2cf9df7489fbeab541f5599
96ayad <96ayad@gmail.com>
2024-07-07 10:40:25 +0300

    openTime: 1720189255,

--- src/config-copy.ts
+++ src/config-copy.ts
@@ -17,14 +17,23 @@ import {
 // export const owner: Keypair = Keypair.fromSecretKey(Uint8Array.from([179,3,16,89,75,94,147,146,107,142,137,163,66,234,236,82,65,183,254,245,47,110,215,216,72,127,119,46,215,249,199,241,11,212,37,99,137,5,243,10,5,224,219,17,144,108,64,250,90,25,98,185,74,111,2,81,54,174,65,121,23,185,59,201])) //adin3
 export const owner: Keypair = Keypair.fromSecretKey(Uint8Array.from(bs58.decode("43EeRipwq7QZurfASn7CnYuJ14pVaCEv7KWav9vknt1bFR6qspYXC2DbaC2gGydrVx4TFtWfyCFkEaLLLMB2bZoT")))
 export const keyPair: Keypair = Keypair.fromSecretKey(Uint8Array.from([
-  179,   3,  16,  89,  75,  94, 147, 146, 107, 142, 137,
-  163,  66, 234, 236,  82,  65, 183, 254, 245,  47, 110,
-  215, 216,  72, 127, 119,  46, 215, 249, 199, 241,  11,
-  212,  37,  99, 137,   5, 243,  10,   5, 224, 219,  17,
-  144, 108,  64, 250,  90,  25,  98, 185,  74, 111,   2,
-   81,  54, 174,  65, 121,  23, 185,  59, 201
+  193,  85,  99, 128,   2, 178, 170,   4, 181, 167,  74,
+   27, 134,  35, 132,  80, 216, 100,  15,  36, 105, 244,
+   35, 144,  98, 179,  31,   7, 178, 208, 107, 197,  20,
+  201, 205,  63, 104, 234, 159,  93,  35, 249,  61,  52,
+  143, 182, 109, 205, 180, 201, 165, 137, 100,  41, 175,
+  206,  54, 162, 204, 138, 248, 107,  10, 122
 ])) //new Keypair()
+export const fee4: Keypair = Keypair.fromSecretKey(Uint8Array.from([136,165,199,41,90,246,101,80,135,94,242,76,185,64,180,136,211,244,120,34,128,73,180,138,71,240,141,136,161,61,166,6,251,231,23,134,175,174,24,42,221,41,108,109,159,85,181,138,68,53,138,25,181,162,139,167,215,247,161,11,75,184,150,196])) //fee4.json
 
+// export const owner: Keypair = Keypair.fromSecretKey(Uint8Array.from([
+//   193,  85,  99, 128,   2, 178, 170,   4, 181, 167,  74,
+//    27, 134,  35, 132,  80, 216, 100,  15,  36, 105, 244,
+//    35, 144,  98, 179,  31,   7, 178, 208, 107, 197,  20,
+//   201, 205,  63, 104, 234, 159,  93,  35, 249,  61,  52,
+//   143, 182, 109, 205, 180, 201, 165, 137, 100,  41, 175,
+//   206,  54, 162, 204, 138, 248, 107,  10, 122
+// ])) //new Keypair()
 export const connection = new Connection('https://api.devnet.solana.com') //<YOUR_RPC_URL>
 // export const connection = new Connection(clusterApiUrl('devnet')) //<YOUR_RPC_URL>
 // export const connection = new Connection('https://boldest-necessary-telescope.solana-devnet.quiknode.pro/d84334700a69839dc8ef8f3f17f82dc0541ed9df/')//https://boldest-necessary-telescope.solana-devnet.quiknode.pro/d84334700a69839dc8ef8f3f17f82dc0541ed9df/
--- src/cpmm/fetchPools.ts
+++ src/cpmm/fetchPools.ts
@@ -6,12 +6,69 @@ import {
   PublicKey,
 } from '@solana/web3.js';
 
+import { initSdk } from '../config';
+
 const owner: Keypair = Keypair.fromSecretKey(Uint8Array.from(bs58.decode("43EeRipwq7QZurfASn7CnYuJ14pVaCEv7KWav9vknt1bFR6qspYXC2DbaC2gGydrVx4TFtWfyCFkEaLLLMB2bZoT")))
 const connection = new Connection('https://api.devnet.solana.com') //<YOUR_RPC_URL>
 
 const programId = new PublicKey('97MQhx2fniaNsQgC4G2M6tLUQBah1etEnhsKe1aMCXbo');
 // const poolId = '9qVb7iFiAoTyFoEYM2ZSBULeHRvBYUhPkpswoESjyUZV';
 const tokenProgram = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+const fetchRpcPoolInfo = async (pool1: string) => {
+  const raydium = await initSdk()
+  // SOL-RAY
+  // const pool1 = '4y81XN75NGct6iUYkBp2ixQKtXdrQxxMVgFbFF9w5n4u'
+  // const pool1 = 'HMa1rFjBY35jW6KvpLJf79XFsThSj1ZJBp8KoVe8nHkB'
+
+  const res = await raydium.cpmm.getRpcPoolInfos([pool1])
+
+  const pool1Info = res[pool1]
+
+  // console.log('pool id: ', pool1)
+  // console.log('SOL-RAY pool price:', pool1Info.poolPrice)
+  // // console.log('cpmm pool infos:', res)
+  // console.log('SOL-SALD pool poolCreator:', `${pool1Info.poolCreator}`) //poolCreator:
+  // console.log('pool1Info.mintA, pool1Info.mintB, pool1Info.mintLp :', 
+  //   `${pool1Info.mintA}`, 
+  //   `${pool1Info.mintB}`, 
+  //   `${pool1Info.mintLp}`
+  // )
+
+  const poolInfo = {
+    poolId: pool1,
+    programId: `${programId}`,
+    poolCreator: `${pool1Info.poolCreator}`,
+    configId: `${pool1Info.configId}`,
+    mintA: `${pool1Info.mintA}`,
+    mintProgramA: `${pool1Info.mintProgramA}`,
+    vaultA: `${pool1Info.vaultA}`,
+    mintB: `${pool1Info.mintB}`,
+    mintProgramB: `${pool1Info.mintProgramB}`,
+    vaultB: `${pool1Info.vaultB}`,
+    bump: pool1Info.bump,
+    status: pool1Info.status,
+    lpAmount: Number(pool1Info.lpAmount),
+    openTime: Number(pool1Info.openTime),
+    poolPrice: pool1Info.poolPrice,
+  }
+
+  console.log('poolInfo: ', poolInfo)
+}
+
+const fetchRpcPoolInfo2 = async (pool1: string) => {
+  const raydium = await initSdk()
+  // SOL-RAY
+  // const pool1 = '4y81XN75NGct6iUYkBp2ixQKtXdrQxxMVgFbFF9w5n4u'
+  // const pool1 = 'HMa1rFjBY35jW6KvpLJf79XFsThSj1ZJBp8KoVe8nHkB'
+
+  const res = await raydium.cpmm.getCpmmPoolKeys(pool1)
+
+//   const pool1Info = res[pool1]
+
+//   console.log('SOL-RAY pool price:', pool1Info.poolPrice)
+  console.log('cpmm pool infos:', res)
+}
 
 async function getParsedProgramAccounts() {
     // const MY_WALLET_ADDRESS = "FriELggez2Dy3phZeHHAdpcoEXkKQVkv6tx3zDtCVP8T";
@@ -72,6 +129,92 @@ async function getProgramAccounts2() {
   console.log('accountKeys: ', accountKeys)
 }
 
+//fetch first 5 pool accounts info only (637 byte(s))
+async function getProgramAccounts3() {
+  const accountsWithoutData = await connection.getProgramAccounts(
+    programId,
+    {
+      dataSlice: { offset: 0, length: 0 },
+      filters: [
+        {
+          dataSize: 637  //637 byte(s)
+        }
+      ]
+    }
+  )
+  
+  const accountKeys = accountsWithoutData.map(account => account.pubkey)
+  // console.log('accountKeys: ', accountKeys)
+
+  const paginatedKeys = accountKeys.slice(0, 5)  //slect only first 5 accounts
+  const accountInfos = await connection.getMultipleAccountsInfo(paginatedKeys)
+  const deserializedObjects = accountInfos.map((accountInfo) => {
+  // put logic to deserialize accountInfo.data here
+  // accountInfo.pubkey
+  console.log('accountInfo?.data: ',accountInfo?.data)
+  })
+  console.log('deserializedObjects: ', deserializedObjects)
+}
+
+//fetch first 4 pool accounts info only (637 byte(s))
+async function getProgramAccounts4() {
+  const raydium = await initSdk();
+  const accountsWithoutData = await connection.getProgramAccounts(
+    programId,
+    {
+      dataSlice: { offset: 0, length: 0 },
+      filters: [
+        {
+          dataSize: 637  //637 byte(s)
+        }
+      ]
+    }
+  )
+  
+  const accountKeys = accountsWithoutData.map(account => account.pubkey)
+  // console.log('accountKeys: ', accountKeys)
+
+  const paginatedKeys = accountKeys.slice(0, 4)  //slect only first 5 accounts
+  
+  paginatedKeys.forEach((progId) => {
+    // const res = await raydium.cpmm.getRpcPoolInfos([`${progId}`])
+    // const res = await raydium.cpmm.getRpcPoolInfos([`${progId}`])
+    // const pool1Info = res[`${progId}`]
+
+    // console.log('SOL-RAY pool price:', pool1Info.poolPrice)
+    // console.log('cpmm pool infos:', res)
+    fetchRpcPoolInfo(`${progId}`);
+    // fetchRpcPoolInfo2(`${progId}`);  //cpmm pool infos: undefined
+  })
+}
+
+async function getProgramAccounts40() {
+  console.log('`${owner.publicKey}`',`${owner.publicKey}`);
+  const accountsWithoutData = await connection.getProgramAccounts(
+    programId,
+    {
+      dataSlice: { offset: 0, length: 0 },
+      filters: [
+        {
+          dataSize: 637  //637 byte(s)
+        },
+        {
+          memcmp:
+            {
+              offset: 642,
+              bytes: `${owner.publicKey}`,
+            }
+        }
+      ]
+    }
+  )
+  
+  const accountKeys = accountsWithoutData.map(account => account.pubkey);
+  console.log('accountKeys: ', accountKeys)
+}
+
 // getParsedProgramAccounts();
 // getProgramAccounts1();
-getProgramAccounts2();
+// getProgramAccounts2();
+// getProgramAccounts3();
+getProgramAccounts4();
