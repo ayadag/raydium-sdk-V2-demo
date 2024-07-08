e155253fe57d6112e7f717da1328d07f1cb2b8a4
ayadag <ayad.ali.g@gmail.com>
2024-07-08 20:26:25 +0300

    fromWeb3JsPublicKey

--- src/cpmm/fetchTokens.ts
+++ src/cpmm/fetchTokens.ts
@@ -12,17 +12,17 @@
 // // import  deprecated from "@metaplex-foundation/mpl-token-metadata";
 // // const splToken = require('@solana/spl-token');
 // // const { Metadata, deprecated } = require('@metaplex-foundation/mpl-token-metadata');
-
+import { web3 } from '@coral-xyz/anchor';
 import {
   Edition,
   fetchDigitalAsset,
   MasterEdition,
   Metadata,
-  mplTokenMetadata,
 } from '@metaplex-foundation/mpl-token-metadata';
 import { Mint } from '@metaplex-foundation/mpl-toolbox';
 import { PublicKey } from '@metaplex-foundation/umi';
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { fromWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters';
 
 // import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
 
@@ -37,12 +37,15 @@ export type DigitalAsset = {
 
 
 async function Token() {
-    // const mint = new web3.PublicKey('So11111111111111111111111111111111111111112'); //SOL
+    // const token = new web3.PublicKey('So11111111111111111111111111111111111111112'); //SOL
+    const token = new web3.PublicKey('Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb'); //SOL
     // const umi = createUmi('https://api.devnet.solana.com', 'processed')
     // Use the RPC endpoint of your choice.
-    const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata())
-    const mint:PublicKey = 'mmnj';
+    // const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata())
+    const umi = createUmi('https://api.devnet.solana.com')
+    const mint = fromWeb3JsPublicKey(token);
     const asset = await fetchDigitalAsset(umi, mint) 
+    console.log('asset: ', asset)
 }
 
 async function Tokens(listType: string, page: number, perPage: number) {
@@ -157,3 +160,55 @@ function getListType(list:string) {
 // Tokens('all', 1, 4); //listType: string, page: number, perPage: number
 
 // TokenDev();
+Token();
+
+
+
+
+// asset:  {
+//   publicKey: 'Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb',  
+//   mint: {
+//     publicKey: 'Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb',
+//     header: {
+//       executable: false,
+//       owner: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',   
+//       lamports: [Object],
+//       rentEpoch: 18446744073709551616n,
+//       exists: true
+//     },
+//     mintAuthority: {
+//       __option: 'Some',
+//       value: 'hCjWAhZNZ4z8gSKhokcZ3HFW761Bb2WhVkmemmajCus'    
+//     },
+//     supply: 1000000000000000000n,
+//     decimals: 9,
+//     isInitialized: true,
+//     freezeAuthority: { __option: 'None' }
+//   },
+//   metadata: {
+//     publicKey: 'CXG5z1rntYQa9d1KZzFf5uyCwRbru5VvdZYL5JTgxb5W',
+//     header: {
+//       executable: false,
+//       owner: 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+//       lamports: [Object],
+//       rentEpoch: 18446744073709551616n,
+//       exists: true
+//     },
+//     key: 4,
+//     updateAuthority: 'hCjWAhZNZ4z8gSKhokcZ3HFW761Bb2WhVkmemmajCus',
+//     mint: 'Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb',
+//     name: 'SALD',
+//     symbol: 'SALD',
+//     uri: '',
+//     sellerFeeBasisPoints: 0,
+//     creators: { __option: 'None' },
+//     primarySaleHappened: false,
+//     isMutable: false,
+//     editionNonce: { __option: 'Some', value: 255 },
+//     tokenStandard: { __option: 'Some', value: 2 },
+//     collection: { __option: 'None' },
+//     uses: { __option: 'None' },
+//     collectionDetails: { __option: 'None' },
+//     programmableConfig: { __option: 'None' }
+//   }
+// }
