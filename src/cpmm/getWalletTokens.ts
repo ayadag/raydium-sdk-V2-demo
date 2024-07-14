bbfe0fa2271c27d9a6db6124c55c85bf376ca195
96ayad <96ayad@gmail.com>
2024-07-14 08:55:09 +0300

    class

--- src/cpmm/getWalletTokens.ts
+++ src/cpmm/getWalletTokens.ts
@@ -377,6 +377,169 @@ async function test() {
     console.log(`meta.uri(${index}) :${meta.uri}`)
   }
 }
+
+class getTokensList {
+  tokens: token[]= [];
+  accounts: any[] = [];
+  tList: any[] = [];
+  meta: any;
+
+  constructor(accounts: any[]){
+    // const tokens:any[];
+    this.accounts = accounts;
+  }
+
+  async getTokenAccounts() {
+    for (let index = 0; index < this.accounts.length; index++) {
+    // this.accounts.forEach((account, i) => {
+    const account = this.accounts[index] //for
+    const parsedAccountInfo:any = account.account.data;
+    const mintAddress:string = parsedAccountInfo["parsed"]["info"]["mint"];
+    const tokenBalance: number = parsedAccountInfo["parsed"]["info"]["tokenAmount"]["uiAmount"];
+
+    this.tokens.push(
+    {
+      address: String(account.pubkey.toString()),
+      mint: String(mintAddress),
+      owner: String(account.account.owner),
+      balance: Number(tokenBalance),
+    }
+    )
+    // tokens[index] = (accounts[index]);
+    // console.log('${account.pubkey.toString()}: ', `${account.pubkey.toString()}`)
+    }
+    //   )
+    // console.log(this.tokens);
+    return this.tokens;
+  }
+
+  
+
+  async token0(tokenId: string) {
+    // const token = new web3.PublicKey('So11111111111111111111111111111111111111112'); //SOL
+    // const token = new web3.PublicKey('Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb'); //SALD
+    const token = new web3.PublicKey(tokenId); //SALD
+    // const umi = createUmi('https://api.devnet.solana.com', 'processed')
+    // Use the RPC endpoint of your choice.
+    // const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata())
+    const umi = createUmi('https://api.devnet.solana.com')
+    // umi.programs.bind('splToken', 'splToken2022');
+  
+    const mint = fromWeb3JsPublicKey(token);
+    try{
+    const asset = await fetchDigitalAsset(umi, mint) 
+    // console.log('asset: ', asset);
+    const tokenData = {
+      publicKey: asset.publicKey,
+      owner: asset.mint.header.owner,
+      mintAuthority: asset.mint.mintAuthority,
+      updateAuthority: asset.metadata.updateAuthority,
+      name: asset.metadata.name,
+      symbol: asset.metadata.symbol,
+      uri: asset.metadata.uri,
+      decimals: asset.mint.decimals,
+      supply: asset.mint.supply,
+      executable: asset.mint.header.executable,
+    }
+    // console.log('tokenData: ', tokenData)
+    return tokenData;
+    } catch { return null }
+  }
+
+  async  token1(tokenId:string, programId:string) {
+    // Retrieve and log the metadata state
+    const metadata = await getTokenMetadata(
+      solanaConnection, // Connection instance
+      new PublicKey(tokenId), // PubKey of the Mint Account
+      'confirmed', // Commitment, can use undefined to use default
+      // TOKEN_2022_PROGRAM_ID, //spl-2022 token
+      // TOKEN_PROGRAM_ID, //spl toke
+      new PublicKey(programId),
+    )
+    // console.log('metadata: ', metadata)
+    return metadata;
+  }
+
+  async get(wallet: string, solanaConnection: Connection, dataSize0: number, dataSize1: number) {
+    const filters0:GetProgramAccountsFilter[] = [
+        {
+          dataSize: dataSize0,    //size of account (bytes) spl-token=165 spl-2022-token=182
+        },
+        {
+          memcmp: {
+            offset: 32,     //location of our query in the account (bytes)
+            bytes: wallet,  //our search criteria, a base58 encoded string
+          },            
+    }];
+
+    const filters1:GetProgramAccountsFilter[] = [
+        {
+          dataSize: dataSize1,    //size of account (bytes) spl-token=165 spl-2022-token=182
+        },
+        {
+          memcmp: {
+            offset: 32,     //location of our query in the account (bytes)
+            bytes: wallet,  //our search criteria, a base58 encoded string
+          },            
+    }];
+
+    const accounts0 = await solanaConnection.getParsedProgramAccounts(
+        TOKEN_PROGRAM_ID, //new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+        {filters: filters0}
+    );
+    // console.log('accounts0: ', accounts0)
+    const accounts1 = await solanaConnection.getParsedProgramAccounts(
+        TOKEN_2022_PROGRAM_ID, //new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
+        {filters: filters1}
+    );
+
+    // const getTL0 = new getTokenList(accounts0);
+    // const list = await getTL.getTokenAccounts('Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb', solanaConnection, 165);
+    const list0 = await this.getTokenAccounts();
+
+    // const getTL1 = new getTokenList(accounts1);
+    const list1 = await this.getTokenAccounts();
+
+    const totalList = [...list0,...list1];
+    // console.log(totalList)
+    return totalList
+  }
+
+  async getU0() {
+    const totalList = await this.get('hCjWAhZNZ4z8gSKhokcZ3HFW761Bb2WhVkmemmajCus', solanaConnection, 165, 182); // wallet= hCjWAhZNZ4z8gSKhokcZ3HFW761Bb2WhVkmemmajCus
+
+    for (let index = 0; index < totalList.length; index++) {
+      const meta0: any = await metadata(totalList[index].mint, totalList[index].owner);
+      const meta1: any = await Token(totalList[index].mint);
+      const meta3 = {
+        name: '',
+        symbol: '',
+        uri: '',
+      }
+      let meta: any = meta0 != null? meta0: meta1 != null? meta1: meta3
+        // this.tList.push(totalList[index], await Token(totalList[index].mint));     
+        // this.tList.push(totalList[index], await metadata(totalList[index].mint, totalList[index].owner));
+        this.tList.push({
+          address: totalList[index].address, 
+          mint: totalList[index].mint, 
+          owner: totalList[index].owner, 
+          balance: totalList[index].balance, 
+          name: String(meta.name) || '',
+          symbol: String(meta.symbol) || '',
+          uri: String(meta.uri) || '',
+        });
+    }
+    return this.tList;
+  }
+
+  async getUri() {
+    // const gU = new geturi();
+    const tL = await this.getU0();
+    console.log(tL)
+  }
+}
+
+
 // console.log('<--spl tokens-->')
 // getTokenAccounts(walletToQuery,solanaConnection);
 // console.log('<--spl-2022 tokens-->')
